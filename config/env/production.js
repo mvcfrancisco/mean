3a0f7e2e5546f8898ddba276acca6ae398ab54f7
mvcfrancisco <mvcfrancisco@yahoo.com>
2017-10-29 23:51:49 +0800

    update db user pw

--- config/env/production.js
+++ config/env/production.js
@@ -10,8 +10,8 @@ module.exports = {
   db: {
     uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
-      user: '',
-      pass: ''
+      user: 'heroku_xgp0501h',
+      pass: 'DTKuQUvZKoucJrJ71Ht3-0_J1aFvjZ2x'
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
